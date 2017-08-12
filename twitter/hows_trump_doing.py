3e47c950bfd93947870918c7217e4778ac9e32a1
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-08-12 01:16:26 -0400

    hows_trump_doing -- changed sentiment analyzer tool. Making tweet updater.

--- twitter/hows_trump_doing.py
+++ twitter/hows_trump_doing.py
@@ -1,5 +1,7 @@
 import re
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import tokenize
 from textblob import TextBlob
 import pandas as pd
 from datetime import datetime
@@ -8,28 +10,41 @@ from matplotlib import style
 style.use("ggplot")
 
 
-df = pd.read_csv("trump_tweets_downloaded.csv")
+df = pd.read_csv("data/trump_tweets_downloaded.csv")
 df = df[df["is_retweet"] == "false"]
 
 len_df = len(df)
 i = 0
 
+
+def preprocess_tweets(text):
+    out = ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split())
+    return out
+
+
+def ampersand_replacer(text):
+    out = re.sub(r"\&amp\;", "&", text)
+    return out
+
+
+sid = SentimentIntensityAnalyzer()
+
 polarities = []
-subjectivities = []
 times = []
 data_list = []
 for i in df.index:
-    tweet = df.loc[i, "text"]
-    trump_tweet = TextBlob(tweet)
+    twt = df.loc[i, "text"]
     time_created = df.loc[i, "created_at"]
     datetime_object = datetime.strptime(time_created, '%m-%d-%Y %H:%M:%S')
-    polarity = trump_tweet.sentiment.polarity
-    subjectivity = trump_tweet.sentiment.subjectivity
+    twt = ampersand_replacer(twt)
+    cleaned_twt = preprocess_tweets(twt)
+    scores = sid.polarity_scores(cleaned_twt)
+    polarity = scores["compound"]
 
     times.append(datetime_object)
     polarities.append(polarity)
-    subjectivities.append(subjectivity)
-    data_list.append((datetime_object, trump_tweet, trump_tweet.sentiment))
+    data_list.append((datetime_object, twt, scores))
 
 
 plt.plot(times[: 20], polarities[: 20])
+plt.show()
--- twitter/update_trump_tweets.py
+++ twitter/update_trump_tweets.py
@@ -1,40 +1,59 @@
 import re
-import pandas as pandas
+import pandas as pd
 import numpy as np
 import twitter
 import json
+import pytz
 import time
-import datetime
+from datetime import datetime, timezone
+from dateutil import tz
 from nltk.tokenize import word_tokenize, sent_tokenize, PunktSentenceTokenizer
-from nltk.corpus import stopwords, state_union
-from nltk import PorterStemmer
 from twitter_api_credential_info import *
 
-ps = PorterStemmer()
 
-stop_words = set(stopwords.words('english'))
-
-
-data_file = 'trump_tweets.txt'
+data_file = 'data/trump_tweets.txt'
+database = 'data/trump_tweets_downloaded.csv'
 screen_name = "realDonaldTrump"
 user_id = 25073877
 
+col_names = ['source', 'text', 'created_at', 'retweet_count', 'favorite_count', 'is_retweet', 'id_str']
 
-def get_existing_data(data_file):
-    with open(data_file) as json_file:
+def get_existing_data(data_path):
+    with open(data_path) as json_file:
         data = json.load(json_file)
     return data
+
+
+def get_source_from_a_tag(a_tag):
+    out = re.findall(r"\<a.+\>([^\<\>]+)\<\/a\>", a_tag)[0]
+    return out
+
+
+def str_to_datetime(time_created):
+    time_created = datetime.strptime(time_created, '%a %b %d %H:%M:%S %z %Y')
+    return time_created
+
+
+def utc_to_est_tz(utc_dt):
+    est_tz = pytz.timezone('US/Eastern')
+    return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=est_tz)
+
+
+def str_from_datetime(created_at):
+    return created_at.strftime('%m-%d-%Y %H:%M:%S')
+
 
 api = twitter.Api(consumer_key=consumer_key,
                   consumer_secret=consumer_secret,
                   access_token_key=access_token_key,
                   access_token_secret=access_token_secret)
 
-print(api.VerifyCredentials())
+
 
 # Trump's Twitter ID == 25073877
 timeline = api.GetUserTimeline(user_id=user_id, count=200)
 new_data = [instance.AsDict() for instance in timeline]
+
 
 try:
     existing_data = get_existing_data(data_file)
@@ -42,16 +61,37 @@ try:
 except:
     data = new_data
 
-with open('trump_tweets.txt', 'w') as outfile:
+
+with open(data_file, 'w') as outfile:
     json.dump(data, outfile)
 
-i=0
+
+new_data = pd.DataFrame(data, columns = col_names)
+
+new_data.loc[:, "source"] = new_data.loc[:, "source"].map(get_source_from_a_tag)
+
+created_times = new_data.loc[:, "created_at"]
+created_times = created_times.map(str_to_datetime)
+created_times = created_times.map(utc_to_est_tz)
+created_times = created_times.map(str_from_datetime)
+new_data.loc[:, "created_at"] = created_times
+
+new_data.loc[new_data.loc[:, 'is_retweet'].notnull(), 'is_retweet'] = "true"
+new_data.loc[new_data.loc[:, 'is_retweet'].isnull(), 'is_retweet'] = "false"
+
+text_overlap = pd.merge(df, new_data, on="id_str", how='inner')
+text_overlap = text_overlap.loc[:, "text"]
+df.query("text not in text_overlap")
+
+
+i = -1
 text = data[i]['text']
 tweet_time = data[i]['created_at']
 time_zone = data[i]['user']['time_zone']
 retweet_count = data[i]['retweet_count']
-word_tokens = word_tokenize(text)
 
-for w in word_tokens:
-    print(ps.stem(w))
+word_tokens = word_tokenize(text)
 sent_tokens = sent_tokenize(text)
+
+
+df = pd.read_csv(database)
