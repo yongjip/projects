f4670d793d7eb7818eda9eea5bdab45ef4c1586f
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-08-14 01:55:39 -0400

    RawTwitterDataMerger -- initial commit. This merges raw Twitter datasets.

---
+++ twitter/raw_twitter_data_merger.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import json
+
+
+def load_json(file_path):
+    with open(file_path, "r") as json_file:
+        data = json.load(json_file)
+    return data
+
+
+class RawTwitterDataMerger:
+    def __init__(self, data=None):
+        self.data = data
+
+    def merge(self, input_path_1, input_path_2, *args):
+        part1 = load_json(input_path_1)
+        part2 = load_json(input_path_2)
+        merged = part1 + part2
+        for path in args:
+            merged += load_json(path)
+        self.data = merged
+        print("Datasets Are Merged")
+        print("Check Data Attribute.")
+
+    def drop_duplicate(self, dedup_by="text", sort_by="id"):
+        df = pd.DataFrame(self.data, dtype='str')
+        df = df.drop_duplicates(dedup_by)
+        df = df.sort_values(sort_by, ascending=False)
+        self.data = df
+        print("Dropped Duplicates by", dedup_by)
+        print("Sorted by", sort_by)
+        print("Data Attribute Contains DataFrame Object")
+
+    def dump_json(self, output_path, orient="records"):
+        self.data.to_json(output_path, orient=orient)
+        print("Output File Orient:", orient)
+        print("Check Directory:", output_path)
+
+
+if __name__ == "__main__":
+    output_file_path = "data/raw_trumps_tweets_all.json"
+    part1 = 'data/raw_trump_tweets_part_1.json'
+    part2 = 'data/raw_trump_tweets_part_2.json'
+
+    merger = RawTwitterDataMerger()
+    merger.merge(part1, part2)
+    merger.drop_duplicate()
+    merger.dump_json(output_file_path)
