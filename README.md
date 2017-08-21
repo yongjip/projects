d99e7e08ec10b887c6c7bbd49dd3bab3342bf906
leeb2018 <lukelee92@gmail.com>
2017-08-21 01:47:53 -0400

    Basic Flask app set up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #Projects
+
+##How to run the app
+Open Terminal, find "*/projects/project_trump" directory, and run following
+'''
+python3 app.py
+''' 
---
+++ project_trump/app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from data_reader import get_recent20
+
+app = Flask(__name__)
+Bootstrap(app)
+DATA_PATH = "../data/cleaned_data/trumps_twitter_data_with_scores.json"
+
+@app.route("/")
+def hello():
+    return render_template("main.html", tweets=get_recent20(DATA_PATH))
+
+
+def main():
+    app.debug = True
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
