447ca4b67ff01403dee3072cd73df394e38f7c81
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-08-24 01:23:08 -0400

    data_reader.py -- changed tabs to spaces

--- trump_project/app/data_reader.py
+++ trump_project/app/data_reader.py
@@ -2,7 +2,6 @@ import json
 
 
 def get_recent20(data_file_path):
-	data_file = open(data_file_path)
-	data = json.loads(data_file.read())
-	
-	return data[0:20]
+    data_file = open(data_file_path)
+    data = json.loads(data_file.read())	
+    return data[0:20]
