5dc7ad7cffe67700ae4911056e86a06f25a0f040
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-09-05 15:42:03 -0400

    app/app.py -- made it get data from DB

--- trump_project/app/app.py
+++ trump_project/app/app.py
@@ -1,16 +1,41 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from data_reader import get_recent20
+from sqlalchemy import MetaData, Table
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import pandas as pd
+from db_url import url
+
+Base = declarative_base()
+
+table_name = 'trump_twitter_data'
+url = url
+
+engine = create_engine(url, client_encoding="utf8", echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+meta = MetaData(bind=engine)
+trump_twitter_data = Table(table_name, meta, autoload=True)
+cols = trump_twitter_data.columns.keys()
+
+q = trump_twitter_data.select().order_by('id DESC').limit(20)
+result = session.execute(q)
+outputs = result.fetchall()
+data = pd.DataFrame(outputs, columns=cols)
+tweets = data.T.to_dict().values()
 
 app = Flask(__name__)
 Bootstrap(app)
-DATA_PATH = "../data/cleaned_data/trumps_twitter_data_with_scores.json"
-DATA_PATH = "/var/www/projects/trump_project/data/cleaned_data/trumps_twitter_data_with_scores.json"
+#DATA_PATH = "../data/cleaned_data/trumps_twitter_data_with_scores.json"
+#DATA_PATH = "/var/www/projects/trump_project/data/cleaned_data/trumps_twitter_data_with_scores.json"
 
 
 @app.route("/")
 def hello():
-    return render_template("main.html", tweets=get_recent20(DATA_PATH))
+    return render_template("main.html", tweets=tweets)
 
 
 def main():
