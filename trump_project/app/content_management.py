045c34aa75f3ba56a60dcf4f2fbb385fe204e68c
COMSCORE_OFFICE\ykim <ykim@comscore.com>
2017-09-07 23:32:31 -0400

    Flask tutorial

---
+++ trump_project/app/content_management.py
@@ -0,0 +1,7 @@
+
+def Content():
+    TOPIC_DICT = {"Basics": [["Introduction to Python", "/introduction-to-python-programming/"],
+                             ["Print functions and String", "/python-tutorial-print-function-string/"],
+                             ["Math basics with Python 3", "/math-basics-python-3-beginner-tutorial/"]],
+                  "Web Dev": []}
+    return TOPIC_DICT
